4347ab82cd5bc2bf45b9d6d5540a0a5361944f4e
alfredes001 <aestrada@serenova.com>
2024-03-19 14:43:13 -0600

    new test scenario

--- cypress/e2e/testSpec.cy.js
+++ cypress/e2e/testSpec.cy.js
@@ -2,6 +2,11 @@
 
 describe('template spec', () => {
   it('passes', () => {
+    // test to test to test
+    cy.visit('https://example.cypress.io')
+  })
+  it('passes', () => {
+    // test to test to test
     cy.visit('https://example.cypress.io')
   })
 })
